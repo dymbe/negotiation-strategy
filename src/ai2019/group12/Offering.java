cb5fabfcb402a7c55c344fd3ea55ceac6efd8e46
F-Parente <franciscolopesparente@gmail.com>
2019-10-10 19:08:19 +0200

    This is a commit

--- src/ai2019/group12/Offering.java
+++ src/ai2019/group12/Offering.java
@@ -1,15 +1,25 @@
 package ai2019.group12;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import genius.core.bidding.BidDetails;
 import genius.core.boaframework.NegotiationSession;
+import genius.core.boaframework.NoModel;
 import genius.core.boaframework.OMStrategy;
 import genius.core.boaframework.OfferingStrategy;
 import genius.core.boaframework.OpponentModel;
+//import sun.awt.shell.Win32ShellFolder2.SystemIcon;
+import genius.core.misc.*;
 
 public class Offering extends OfferingStrategy {
 
+	private double time_threshold = 0.95;
+	private double utility_threshold = 0.95;
+
+
+	
 	public Offering() {
 	}
 
@@ -26,14 +36,48 @@ public class Offering extends OfferingStrategy {
 	
 	@Override
 	public BidDetails determineOpeningBid() {
-		return negotiationSession.getMinBidinDomain();
+		return negotiationSession.getMaxBidinDomain();
 	}
 
 	@Override
 	public BidDetails determineNextBid() {
-		return negotiationSession.getMaxBidinDomain();
-	}
+		double time = negotiationSession.getTime();
+		double relative_util_threshold = utility_threshold*negotiationSession.
+				getMaxBidinDomain().getMyUndiscountedUtil();
+	
+		
+		if (!(opponentModel instanceof NoModel)) {
+			if (time <= time_threshold) {
+				//next_bid =
+				bids_above_threshold();
+				
+			}
 
+			
+			
+		} else {
+			
+			
+		}
+		return nextBid;
+		
+	}
+	
+
+	public void bids_above_threshold() {
+		
+		Range our_range = new Range(negotiationSession.getMaxBidinDomain().getMyUndiscountedUtil()*utility_threshold, negotiationSession.getMaxBidinDomain().getMyUndiscountedUtil()+0.1) ;
+		
+		//List<BidDetails> possible_bids = negotiationSession.getOutcomeSpace().getBidsinRange(our_range) ;
+		
+		System.out.print(negotiationSession.getMaxBidinDomain().getMyUndiscountedUtil()*utility_threshold);
+		System.out.print(negotiationSession.getMaxBidinDomain().getMyUndiscountedUtil());
+		//System.out.print(negotiationSession.getOutcomeSpace().getBidsinRange(our_range));
+		
+		
+	}
+	
+	
 	@Override
 	public String getName() {
 		return "BraindeadCabbageOS";
